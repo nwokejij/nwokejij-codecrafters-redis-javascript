1a389f47ad51f87163603d7007e4f26ba2b2bb16
Jonathan Nwokeji <nwokejij@unc.edu>
2024-06-05 22:11:58 -0500

    pass stage

--- app/main.js
+++ app/main.js
@@ -18,7 +18,6 @@ const server = net.createServer((connection) => {
 function parseRedisResponse(data) {
     const type = data.charAt(0);
     const content = data.slice(1).trim();
-  
     switch (type) {
         case '+': // Simple string
             return content;
@@ -37,21 +36,37 @@ function parseRedisResponse(data) {
             stringArray = bulkStrings.split('\r\n');
             stringArrayLen = stringArray.length;
             noNewLine = [];
+            
             for (let i = 0; i < stringArrayLen; i++){
                 if (stringArray[i] == "ECHO"){
                     noNewLine.pop();
                     continue;
                 } else if (stringArray[i] == "PING"){
                     return "+PONG\r\n";
+                } else if (stringArray[i] == "SET"){
+                    dictionary[stringArray[i+2]] = stringArray[i + 4];
+                    return "+OK\r\n";
+                } else if (stringArray[i] == "GET"){
+                    if (!(stringArray[i+2] in dictionary)) {
+                        return getBulkString(null);
+                    }
+                    return getBulkString(dictionary[stringArray[i+2]]);
                 } else {
                     noNewLine.push(stringArray[i]);
                 }
-            }
+                }
             strings = noNewLine.join("\r\n");
             console.log(strings);
             return strings;
         default:
             throw new Error('Invalid Redis response');
     }
+}
+function getBulkString(string){
+    if (string == null){
+        return "\$$-1\r\n"
+    }
+
+    return f`\$$${string.length}\r\n${string}\r\n`
 }
 server.listen(6379, "127.0.0.1");
