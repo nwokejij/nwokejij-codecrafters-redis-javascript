78053ca0abfa014c8a355d926fa7ca42d641a2e2
Jonathan Nwokeji <nwokejij@unc.edu>
2024-06-05 21:35:01 -0500

    pass stage

--- app/main.js
+++ app/main.js
@@ -7,8 +7,6 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((connection) => {
   // Handle connection
     connection.on('data', (data) => {
-        // connection.write("+PONG\r\n");
-
         const command = data.toString();
         const message = parseRedisResponse(command);
         connection.write(message);
@@ -33,24 +31,12 @@ function parseRedisResponse(data) {
             // console.log("Length: " + length);
             return data.slice(data.indexOf('\r\n') + 2, data.indexOf('\r\n') + 2 + length);
         case '*': // Array
-        //*2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n$11\r\n<11char>\r\n
-        //how to parse this
-        // *2 $4 ECHO $3 hey $X <word>
-        // 2, 4, 6
-        // $4\r\nECHO
-        // *1\r\n$4\r\nPING\r\n
-        // *1 $4 PING
             words = data.split('\r\n');
-            // const elements = parseInt(content, 10);
-            console.log("Data:" + data);
             delimiter = data.indexOf('\r\n');
-            console.log("Index: " + delimiter);
             bulkStrings = data.slice(delimiter+2);
-            console.log("string:" + bulkStrings);
             stringArray = bulkStrings.split('\r\n');
             stringArrayLen = stringArray.length;
             noNewLine = [];
-            console.log("Message:" + stringArray[0]);
             for (let i = 0; i < stringArrayLen; i++){
                 if (stringArray[i] == "ECHO"){
                     noNewLine.pop();
@@ -63,10 +49,6 @@ function parseRedisResponse(data) {
             }
             strings = noNewLine.join("\r\n");
             console.log(strings);
-            // let index = data.indexOf('\r\n'); // 3
-            
-            // console.log("BulkStrings: " + bulkStrings + ".");
-            // const subResponse = parseRedisResponse(bulkStrings);
             return strings;
         default:
             throw new Error('Invalid Redis response');
