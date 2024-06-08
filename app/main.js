de7611d9b5b1b67607f91585daa01eeaff3ccf7d
Jonathan Nwokeji <nwokejij@unc.edu>
2024-06-07 23:40:45 -0500

    pass stage

--- app/main.js
+++ app/main.js
@@ -48,9 +48,8 @@ function parseRedisResponse(data) {
                         return getBulkString("role:slave");
 
                     }
-                    s = "*3/r/n";
                     
-                     return getBulkString("master_replid:8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb") + getBulkString("role:master") + getBulkString("master_repl_offset:0");
+                     return getBulkString("role:master") + "\n" + getBulkString("master_replid:8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb") + "\n"+ getBulkString("master_repl_offset:0");
                 }
                 else if (stringArray[i] == "ECHO"){
                     noNewLine.pop();
