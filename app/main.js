82178610d30492e910f5fef95bff2c2c8e69258f
Jonathan Nwokeji <nwokejij@unc.edu>
2024-06-11 17:23:16 -0500

    pass stage

--- app/main.js
+++ app/main.js
@@ -12,7 +12,7 @@ if (isSlave != -1){
         client.on('data', (data) => {
             setTimeout(() => {
                 client.write("*3\r\n" + getBulkString("PSYNC") + getBulkString("?")+ getBulkString("-1"));
-            }, 5);
+            }, 4);
             client.write("*3\r\n"+ getBulkString("REPLCONF") + getBulkString("listening-port") + getBulkString(PORT));
             client.write("*3\r\n"+ getBulkString("REPLCONF") + getBulkString("capa") + getBulkString("psync2")); 
             
