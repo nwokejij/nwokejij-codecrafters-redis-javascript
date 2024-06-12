5356f64e2c37908c72afdfe8cc26e2b4e1ce56fb
Jonathan Nwokeji <nwokejij@unc.edu>
2024-06-11 19:22:23 -0500

    pass stage

--- app/main.js
+++ app/main.js
@@ -76,10 +76,11 @@ const server = net.createServer((connection) => {
         console.log("have we reached here");
         connection.write(message);
         if (command.indexOf("PSYNC") != -1){
-            console.log("Entered PSYNC stage");
             const binaryBuffer = Buffer.from("524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2");
             const binaryString = binaryBuffer.toString('binary');
-            connection.write(getBulkString(binaryString).slice(0, -2));
+            const rdbFile = getBulkString(binaryString).slice(0, -2);
+            console.log("RDBFile:" + rdbFile);
+            connection.write();
         }
         
     })
