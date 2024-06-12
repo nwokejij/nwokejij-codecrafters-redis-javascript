6bd13754053fea587524046af71973a929f3c3d9
Jonathan Nwokeji <nwokejij@unc.edu>
2024-06-11 19:26:07 -0500

    pass stage

--- app/main.js
+++ app/main.js
@@ -78,6 +78,7 @@ const server = net.createServer((connection) => {
         if (command.indexOf("PSYNC") != -1){
             const binaryBuffer = Buffer.from("524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2", "hex");
             const binaryString = binaryBuffer.toString('binary');
+            console.log("Binary String: "+ binaryString);
             const rdbFile = getBulkString(binaryString).slice(0, -2);
             console.log("RDBFile:" + rdbFile);
             connection.write(rdbFile);
