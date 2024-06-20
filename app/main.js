d913813fd6599aadd67d37c774b5f6009c6d8440
Jonathan Nwokeji <nwokejij@unc.edu>
2024-06-20 14:03:27 -0500

    pass stage

--- app/main.js
+++ app/main.js
@@ -51,7 +51,7 @@ console.log("Logs from your program will appear here!");
 const replica = net.createServer((connection) => {
 
     connection.on('data', (data) => {
-
+        console.log("i'd be surprised if we entered here");
     })
 });
 
@@ -79,7 +79,6 @@ const server = net.createServer((connection) => {
     connection.on('data', (data) => {
         const command = data.toString();
         const message = parseRedisResponse(command);
-        console.log("Message given:" + message);
         connection.write(message);
         if (command.indexOf("PSYNC") != -1){
             const hex = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2";
