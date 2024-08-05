b871961eee1c181c47bed74855ea3ff1640fef86
Jonathan Nwokeji <nwokejij@unc.edu>
2024-08-05 13:03:32 -0500

    pass stage

--- app/main.js
+++ app/main.js
@@ -246,13 +246,13 @@ const server = net.createServer((connection) => {
             connection.write("-ERR EXEC without MULTI\r\n");
         } else {
         
-        let cmd = `*${execQueue.length-1}\r\n`
-        for (let i = 1; i < execQueue.length; i++){
+        let cmd = `*${execQueue.length}\r\n`
+        for (let i = 0; i < execQueue.length; i++){
             if (typeof execQueue[i] === "string"){
                 cmd += `$${execQueue[i].length}\r\n${execQueue[i]}\r\n`
             } else {
-                res = `:${execQueue[i]}\r\n`
-                cmd += res;
+                valAsString = execQueue[i].toString();
+                cmd += getBulkString(valAsString);
             }
             
         }
