a733494b46a7d25fa700d8de355a2f0293d8acf0
Jonathan Nwokeji <nwokejij@unc.edu>
2024-08-09 11:15:12 -0400

    pass stage

--- app/main.js
+++ app/main.js
@@ -248,12 +248,8 @@ const server = net.createServer((connection) => {
         
         let cmd = `*${execQueue.length}\r\n`
         for (let i = 0; i < execQueue.length; i++){
-            if (typeof execQueue[i] === "string"){
-                cmd += `$${execQueue[i].length}\r\n${execQueue[i]}\r\n`
-            } else {
-                cmd += `:${execQueue[i]}\r\n`
-            }
-            
+            valAsString = execQueue[i].toString()
+            cmd += `$${valAsString.length}\r\n${valAsString}\r\n`
         }
         
         // console.log("cmd", cmd);
